65582e0453cc4e5f5a03925ae7873adac49aa1eb
woohwan <whaprk70@naver.com>
2023-12-06 14:11:06 +0900

    first commit

---
+++ opensearch-client-ssl.py
@@ -0,0 +1,24 @@
+from opensearchpy import OpenSearch
+
+host = 'localhost'
+port = 9200
+auth = ('admin', 'admin') # For testing only. Don't store credentials in code.
+ca_certs_path = 'root-ca.pem' # Provide a CA bundle if you use intermediate CAs with your root CA.
+
+# Optional client certificates if you don't want to use HTTP basic authentication.
+client_cert_path = 'kirk.pem'
+client_key_path = 'kirk-key.pem'
+
+# Create the client with SSL/TLS enabled, but hostname verification disabled.
+client = OpenSearch(
+    hosts = [{'host': host, 'port': port}],
+    http_compress = True, # enables gzip compression for request bodies
+    http_auth = auth,
+    client_cert = client_cert_path,
+    client_key = client_key_path,
+    use_ssl = True,
+    verify_certs = True,
+    ssl_assert_hostname = False,
+    ssl_show_warn = False,
+    ca_certs = ca_certs_path
+)
